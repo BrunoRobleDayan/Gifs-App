385a03a77ae07c04a213d4a278fa0e329aa30cb8
Bruno Roble <brunoroble11@gmail.com>
2023-04-28 15:51:39 -0300

    Fin de gifs App

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+
+@Component({
+  selector: 'share-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+
+constructor(private GifsService: GifsService){}
+
+get tags(): string[] {
+  return this.GifsService.tagHistory;
+}
+
+searchTag(tag: string){
+  this.GifsService.searchTag(tag);
+}
+
+}
